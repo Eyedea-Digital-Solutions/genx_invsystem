13cc739fbd5e3f18f4b5e0bd78a05d3c0c65ae50
Chantel Chikanya <chantelchikanya@outlook.com>
2026-03-03 17:13:25 +0200

    build

--- inventory_system/wsgi.py
+++ inventory_system/wsgi.py
@@ -2,3 +2,16 @@ import os
 from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'inventory_system.settings')
 application = get_wsgi_application()
+
+try:
+    from django.contrib.auth import get_user_model
+    User = get_user_model()
+    if not User.objects.filter(username='admin').exists():
+        User.objects.create_superuser(
+            username='admin',
+            email='admin@genx.com',
+            password='admin1234',
+            role='admin',
+        )
+except Exception:
+    pass 
